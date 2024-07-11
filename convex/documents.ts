9c4daf818edde5c3d4307a12e0fb117bb7d8697b
dheeujjxs <dheerajgoswami@gmail.com>
2024-07-11 09:45:04 +0530

    scoping data to user identifer

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,11 +1,21 @@
 
 
 import { mutation , query } from "./_generated/server";
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 
 export const getDocuments = query({
     async handler(ctx) {
-        return await ctx.db.query('documents').collect()
+        const userId =(await ctx.auth.getUserIdentity())?.tokenIdentifier
+        console.log(userId)
+if(!userId){
+    return []
+}
+        return await ctx.db.query('documents')
+        .withIndex('by_tokenIdentifier', (q) => q.eq('tokenIdentifier' , 
+                userId
+        )).collect()
+
+        
     }
 })
 
@@ -16,8 +26,14 @@ export const createDocument = mutation({
         title: v.string(),
     },
     async handler(ctx,args){
+        const userId =(await ctx.auth.getUserIdentity())?.tokenIdentifier
+                console.log(userId)
+        if(!userId){
+            throw new ConvexError('Noy authnicated')
+        }
         await ctx.db.insert('documents', {
-          title: args.title
+          title: args.title,
+          tokenIdentifier: userId
         })
     }
-})
+});
